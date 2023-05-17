2706e669bb5a21f50d8e680edb95c17c9bb6a766
remciszewski <remciszewski@icloud.com>
2023-05-17 21:22:17 +0200

    full project

---
+++ RomanToInt/Program.cs
@@ -0,0 +1,17 @@
+﻿using RomanToInt;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Roman numerals: \nI = 1\nV = 5\nX = 10\nL = 50\nC = 100\nD = 500\nM = 1000");
+
+        Console.WriteLine("Enter a Roman numeral: ");
+        string RomanToNumeral = Console.ReadLine().ToUpper();
+
+        int result = RomanToIntConverter.RomanToInt(RomanToNumeral);
+        Console.WriteLine("The equivalent integer value is: " + result);
+    }
+
+}
---
+++ RomanToInt/RomanToIntConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanToInt
+{
+    public class RomanToIntConverter
+    {
+        public static int RomanToInt(string s)
+        {
+            Dictionary<char, int> romanMap = new Dictionary<char, int>()
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 }
+            };
+
+            int result = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i + 1 < s.Length && romanMap[s[i]] < romanMap[s[i + 1]])
+                {
+                    result -= romanMap[s[i]];
+                }
+                else
+                {
+                    result += romanMap[s[i]];
+                }
+            }
+
+            return result;
+        }
+    }
+}
